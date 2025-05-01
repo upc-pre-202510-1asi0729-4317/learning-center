8dee54d674821a3d91f72836f03750923d55cfa3
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-04-30 19:30:31 -0500

    feat: add router, add i18n, add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,26 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideTranslateService, TranslateLoader} from '@ngx-translate/core';
+import {HttpClient, provideHttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+const httpLoaderFactory: (http: HttpClient) =>
+  TranslateLoader = (http: HttpClient) =>
+    new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateHttpLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './public/pages/home/home.component';
+import {AboutComponent} from './public/pages/about/about.component';
+import {PageNotFoundComponent} from './public/pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
