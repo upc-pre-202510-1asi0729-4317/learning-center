eb4ba55dc9decdf3f81332777c4be5726229c7d1
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-05-10 17:06:39 -0500

    feat: add learning BC

---
+++ src/app/learning/services/course.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from '../../shared/services/base.service';
+import {Course} from '../model/course.entity';
+import {environment} from '../../../environments/environment';
+
+const coursesResourceEndpointPath = environment.coursesEndpointPath;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService extends BaseService<Course>{
+
+  constructor() {
+    super();
+    this.resourceEndPoint = coursesResourceEndpointPath;
+  }
+
+}
