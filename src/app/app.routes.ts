2c6603f64120d2fcacc994e18a17893d66e31a10
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-05-10 15:55:37 -0500

    feat: add lazyload routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './public/pages/home/home.component';
-import {AboutComponent} from './public/pages/about/about.component';
-import {PageNotFoundComponent} from './public/pages/page-not-found/page-not-found.component';
+
+const AboutComponent = () => import('./public/pages/about/about.component').then(m => m.AboutComponent);
+const PageNotFoundComponent = () =>  import('./public/pages/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent);
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'about', loadComponent: AboutComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', loadComponent: PageNotFoundComponent }
 ];
