619a9ec4226ebc250ae4a39bae20f776d1ca75c1
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-05-15 22:11:54 -0500

    feat: add course-management

---
+++ src/app/learning/components/course-management/course-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-management',
+  imports: [],
+  templateUrl: './course-management.component.html',
+  styleUrl: './course-management.component.css'
+})
+export class CourseManagementComponent {
+
+}
